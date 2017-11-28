5484a4c492ef0b348e48931cc006ece168a30c94
CyborgPiggy <31074648+datroof@users.noreply.github.com>
2017-11-28 09:29:05 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+# Matthew
+
+a = 4
+b = 3
+
+print(3+5)
+print(5-3)
+print(6/2)
+print(3 ** 2)
+
+print("Try to figure out how this works")
+print(13 % 2)
+
+# The sign is a modulus. It finds the remainder.
+
+car_type = "BPW"
+car_name = "Weeaboo steve"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("i have a car called %s. It's pretty nice."% car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type))
+
+# Here is where fancy happens.
+print("What is your name?")
+name = input(">_ ")
+print("Hello %s." % name)
+
+age = input("How old are you")
+print("%s?! That's really old! You should die now." %age) 

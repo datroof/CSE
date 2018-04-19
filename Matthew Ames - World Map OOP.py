014017dbb6e08474aa79113a93ffae14f59cad8c
CyborgPiggy <31074648+datroof@users.noreply.github.com>
2018-04-19 09:45:49 -0700

    I finished it.
    I finished it.

--- Matthew Ames - World Map OOP.py
+++ Matthew Ames - World Map OOP.py
@@ -4,38 +4,58 @@ class Settings:
     real_penny = False
     quest = False
     flashlight_on = False
+    quest_num = 0
+    gave_penny = False
+    gave_quarter = False
+    gave_dime = False
+    gave_nickel = False
+    pls_print = True
+    num_flash = 0
+    add_flash_b = True
+    add_flash_f = True
+    took_penny = False
 
 
-print("Welcome to my game, to the world of Starving Hobo.\n"
-      "\n"
-      "Jest letting you know now, the only actions are:\n"
-      "use\n"
-      "talk\n"
-      "take\n"
-      "give\n"
-      "open\n"
-      "i (you type in i to check your inventory)\n"
-      "\n"
-      "You need to type in an action by itself.\n"
-      "\n"
-      "The game will then ask you to specify your request.\n"
-      "\n"
-      "For example:\n"
-      "player: use\n"
-      "game: Use what?\n"
-      "player: hat\n"
-      "game: You used hat.\n"
-      "\n"
-      "You need to type in a direction to move that way.\n"
-      "These directions are:\n"
-      "north/n\n"
-      "west/w\n"
-      "south/s\n"
-      "east/e\n"
-      "\n"
-      "Now that you know how to play, let's begin your adventure as a Starving Hobo.\n"
-      "P.S... Scroll up to read the directions.\n"
-      "")
+print("Welcome to the world of Starving Hobo.")
+response = input("Would you like to learn the rules of the game?\n"
+                 ""
+                 "yes/no?")
+if response.lower() == "yes":
+    print("Welcome to my game, to the world of Starving Hobo.\n"
+          "\n"
+          "Jest letting you know now, the only actions are:\n"
+          "use\n"
+          "talk\n"
+          "take\n"
+          "give\n"
+          "open\n"
+          "i (you type in i to check your inventory)\n"
+          "\n"
+          "You need to type in an action by itself.\n"
+          "\n"
+          "The game will then ask you to specify your request.\n"
+          "\n"
+          "For example:\n"
+          "player: use\n"
+          "game: Use what?\n"
+          "player: hat\n"
+          "game: You used hat.\n"
+          "\n"
+          "You need to type in a direction to move that way.\n"
+          "These directions are:\n"
+          "north/n\n"
+          "west/w\n"
+          "south/s\n"
+          "east/e\n"
+          "\n"
+          "Now that you know how to play, let's begin your adventure as a Starving Hobo.\n"
+          "P.S... Scroll up to read the directions.\n"
+          "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+          "\n")
+else:
+    print("Ok then. Good luck.\n"
+          "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+          "\n")
 
 
 class Room(object):
@@ -109,7 +129,11 @@ class Penny(Coin):
         super(Penny, self).__init__('penny', taken, used, dropped, given)
 
     def take(self):
-        self.taken = True
+        if Settings.took_penny is True:
+            print("There are only fake pennies left.")
+        elif Settings.took_penny is False:
+            self.taken = True
+            Settings.took_penny = True
 
     def give(self):
         self.given = True
@@ -156,6 +180,8 @@ class Pizza(Item):
         print("You ate the pizza.")
         bum_joe.hungry = False
         self.name = "pizza crumbs"
+        bum_joe.luck += 1
+        print("Your luck is %s" % bum_joe.luck)
 
 
 class Key(Item):
@@ -223,25 +249,36 @@ class Clothes(Item):
 class Highheels(Clothes):
     def __init__(self, taken, used, dropped, equipped):
         super(Highheels, self).__init__('high heels', taken, used, dropped, equipped)
+        self.used = False
 
     def use(self):
-        super(Highheels, self).equip()
-        print("You put on the high heels. You feel fabulous.")
-        bum_joe.luck += 1
-        print("Your luck is %s" % bum_joe.luck)
+        if self.used is True:
+            print("You already used that.")
+        elif self.used is False:
+            super(Highheels, self).equip()
+            print("You put on the high heels. You feel fabulous.")
+            self.used = True
+            bum_joe.luck += 1
+            print("Your luck is %s" % bum_joe.luck)
 
 
 class Hat(Clothes):
     def __init__(self, taken, used, dropped, equipped):
         super(Hat, self).__init__('hat', taken, used, dropped, equipped)
         self.equipped = False
+        self.used = False
 
     def use(self):
-        super(Hat, self).equip()
-        self.equipped = True
-        bum_joe.luck += 1
-        print("You put on the hat. You feel fabulous.")
-        print("Your luck is %s" % bum_joe.luck)
+        if self.used:
+            print("You already used that.")
+        elif self.used is False:
+            super(Hat, self).equip()
+            self.equipped = True
+            self.used = True
+            bum_joe.luck += 1
+            print("You put on the hat. You feel fabulous.")
+            print("Your luck is %s" % bum_joe.luck)
+
 
 
 class Button(Item):
@@ -257,6 +294,7 @@ class Button(Item):
         else:
             self.used = True
             print("You press the button. Nothing happens, but you feel luckier.")
+
 
 secinv = []
 
@@ -299,8 +337,8 @@ george = Character("He has a trench coat and a key around his neck.", "George", 
                    "I'm gonna tag along with you.", None, False)
 
 hdum = Room("Home Dumpster.", "Every hobo in the Istan Allies owns a dumpster, this one's yours.\n In it is a chest that"
-                             " you really wanna open. There are allies to the North, South, East and West of you\n"
-                             ". There's also a hat. You like the color.",
+                           " you really wanna open. There are allies to the North, South, East and West of you\n"
+                           ". There's also a hat. You like the color.",
             False, hat, "na", "rdum", "dwpc", "ugkn", False, None)
 na = Room("North Alley.", "To the west of you is the entrance to an Abandoned Apt. The Alley continues north.", False,
           'none', "corner", None, "hdum", "abap", False, None)
@@ -320,13 +358,13 @@ pero = Room("Penny Room.", "The door lead you to a small room, with nothing but 
                            ",\n all of the pennies appear to be fake. There's a sign on the far wall that reads,\n'"
                            "One of these is real'.",
             False, "fake penny", None, "fdum", None, None, False, None)
-sink = Room("Sink.", "The sink is really weird. It has no knobs to turn the water one, and the faucet the water\n"
+sink = Room("Sink.", "The sink is really weird. It has no knobs to turn the water on, and the faucet the water\n"
                      "would've come out of is the size of a baseball. There must be a way to turn it on.", False,
             nickel, None, "ghto", None, None, False, None)
 mwk = Room("Man with key.", "Upon further inspection, the figure is actually a man in a black trench coat \n"
                             "with a mask covering his face. Around his neck is a chain with a key around it.\n"
                             "It might be the key you're looking for. You think his name is George",
-           False, chestkey, "abshe", None, "rdum", "corner", False, george)
+           False, None, "abshe", None, "rdum", "corner", False, george)
 abshe = Room("Abandoned shop entrance.", "The door is open and when looking inside you can't see anything. \n"
                                          "There is a button on the wall to the west of you with a sign above\n"
                                          " it that says 'Warning: Pointless button. You wanna press it.", False, None,
@@ -335,7 +373,8 @@ absh = Room("Abandoned shop.", "The shop is mostly empty except for a cash regis
                                "is a single quarter.", True, quarter, None, None, "abshe", None, False, None)
 pobu = Room("Pointless button.", "The button is big and red. You really want to push it.",
             False, button, None, "abshe", None, None, False, None)
-rdum = Room("Red Dumpster.", "When looking in the red dumpster, you see there is a flashlight.", False,
+rdum = Room("Red Dumpster.", "When looking in the red dumpster, you see there is a flashlight.\n"
+                             " To the north of you is a mysterious looking figure staring at you.", False,
             flashlight, "mwk", None, None, "hdum", False, None)
 dwpc = Room("Door with pass code.", "On the door is an input thingy with a dialpad. You input the code you got(1234). "
                                     "The door opens to reveal a small room to the east.",
@@ -355,7 +394,7 @@ def use_menu():
     use_response = input("Use what?")
     found = False
     try:
-        if use_response == 'button' and button.used is True:
+        if use_response.lower() == 'button' and button.used is True:
             print("You broke the button when you pressed it, so you can't do it again.")
             found = True
         elif use_response == 'button':
@@ -410,7 +449,6 @@ def open_menu():
         exit(0)
 
 
-
 current_node = hdum
 directions = ['north', 'south', 'east', 'west']
 short_directions = ['n', 's', 'e', 'w']
@@ -426,36 +464,62 @@ current_chr = bum_joe
 
 
 while True:
-    if flashlight and batteries in inventory:
+    if Settings.num_flash == 2:
         Settings.flashlight_on = True
+    if flashlight in inventory and Settings.add_flash_f is True:
+        Settings.num_flash += 1
+        Settings.add_flash_f = False
+    if batteries in inventory and Settings.add_flash_b is True:
+        Settings.num_flash += 1
+        Settings.add_flash_b = False
     if bum_joe.luck >= 2:
         Settings.real_penny = True
     if bum_joe.luck == 4:
         Settings.win_condition = True
-    if current_node == pero and Settings.real_penny is True:
+    if current_node == pero and Settings.real_penny is True and Settings.took_penny is False:
         print()
         pero.objects = penny
-
-    if penny and nickel and quarter and dime in secinv:
+    else:
+        pero.objects = None
+    if penny.given and Settings.gave_penny is False:
+        Settings.quest_num += 1
+        Settings.gave_penny = True
+    if dime.given and Settings.gave_dime is False:
+        Settings.quest_num += 1
+        Settings.gave_dime = True
+    if quarter.given and Settings.gave_quarter is False:
+        Settings.quest_num += 1
+        Settings.gave_quarter = True
+    if nickel.given and Settings.gave_nickel is False:
+        Settings.quest_num += 1
+        Settings.gave_nickel = True
+    if Settings.quest_num == 4:
+        print("George speaks to you.")
         Settings.quest = True
-        pls_print = True
-        if pls_print is True:
+        if Settings.pls_print is True:
             print("\n"
                   "Thank you for giving me all the items I need to start the ritual.\n"
                   "The ritual to end all the knuckles.\n"
                   "Their reign of terror ends here!\n"
                   "\n"
-                  "George disappears. You feel conflicted about these knuckles people... but you're too tired to care.\n"
+                  "George disappears. You feel conflicted about these knuckles people...but you're too tired to care.\n"
                   "\n"
                   "You got the key.\n"
                   "\n")
             inventory.append(chestkey)
-            pls_print = False
-            secinv.remove(penny and nickel and quarter and dime)
+            Settings.quest_num = 0
+            Settings.pls_print = False
+
     elif current_node == pero and bum_joe.luck >= 2:
         print("The door lead you to a small room, with nothing but pennies in it. Upon further inspection"
               ",\n all of the pennies appear to be fake. There's a sign on the far wall that reads,\n'"
               "One of these is real'.")
+    elif current_node == absh and current_node.objects is None:
+        print("You are in the abandoned shop. Apparently, \n"
+              "the only thing in here was a quarter. Odd.")
+    elif current_node == abap and current_node.objects is None:
+        print("You are in the abandoned apt. Apparently, \n"
+              "the only thing in here was a dime. Odd.")
     elif current_node == pero and penny in inventory:
         print("The room now has nothing but fake pennies in it.")
     elif current_node == pero and penny in secinv:
@@ -466,7 +530,7 @@ while True:
     elif current_node == bioc and current_node.objects is None:
         print("You are in the room where you got the batteries. \n"
               "There is nothing else of interest here.")
-    elif current_node == sink and Settings.toilet_used is True:
+    elif current_node == sink and Settings.toilet_used is True and nickel not in inventory:
         print("You look in the sink where you heard the clinking sound. You see there is\n"
               " a nickel. How this actually worked, you'll probably never know, nor will\n"
               " you question it.")
@@ -475,11 +539,10 @@ while True:
     elif current_node == ugkn and code in inventory:
         print("You are where the mysterious creature used to be. You hope he finds da wae.")
     elif current_node == rdum and flashlight in inventory:
-        print("You are at Red Dumpster.")
+        print("You are at Red Dumpster. To the north of you is a mysterious\n"
+              " looking figure staring at you.")
     elif current_node.dark and Settings.flashlight_on is False:
         print("It is too dark to see. You need flashlight and batteries.")
-    elif current_node == pero and bum_joe.luck == 3:
-        current_node.objects = penny
     elif current_node == hdum and hat in inventory:
         print("You are at Home Dumpster. Every hobo in the Istan Allies owns a dumpster, this one's yours.\n"
               "In it is a chest that you really want to open. There are allies to the North, South, East, and"
@@ -495,14 +558,6 @@ while True:
     else:
         print("You are at", current_node.name, current_node.description)  # change
     command = input('>_').lower().strip()
-
-    if command in drop:
-        response = input("Drop what?")
-        if response in inventory:
-            inventory.remove(response)
-            print("You dropped %s" % response)
-        else:
-            print("You can't drop what you don't have.")
 
     if command in talk:
         response = input("To who?").lower().strip()
@@ -532,17 +587,24 @@ while True:
     if command in take:
         try:
             response = input("Take what?")
-            if response == 'button':
+            if response.lower() == 'button':
                 print("You can't take that.")
+            elif response.lower() == 'chest key':
+                print("You can't steal from poor people, meanie.")
+            elif response.lower() == 'key':
+                print("You can't still from poor people, meanie.")
             elif response == current_node.objects.name:
                 current_object = current_node.objects
+                inventory.append(current_object)
+                current_node.objects = None
+                print("You take %s" % current_object.name)
             else:
                 raise NameError
-            print("You take %s" % current_object.name)
-            inventory.append(current_object)
-            current_node.objects = None
-            if response == "penny" and current_node == pero and bum_joe.luck >= 2:
+            if response.lower() == "penny" and current_node == pero and bum_joe.luck >= 2:
                 print("You feel lucky. You pick up the first penny you see. It turns out to be real.")
+                Settings.took_penny = True
+            if response.lower() == "penny" and penny in inventory or penny in secinv:
+                print("There are only fake pennies left.")
         except NameError:
             print("It's not there.")
         except AttributeError:
